eed2409e565f2fc98bec69af0a6eecbb1361c06c
fscheugenpflug <fs.scheugenpflug@hotmail.de>
2018-04-25 18:49:12 +0200

    dom manipultation for dashboard almost done

--- public/javascripts/my-team.js
+++ public/javascripts/my-team.js
@@ -3,12 +3,24 @@
 function mainTeamInfo (team) {
   const teamApi = 'https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=';
   const teamInfoTotal = team;
-  const divElement = document.createElement('div');
+
+  const aElement = document.createElement('a');
   // const searchButton = document.querySelector('.search-btn');
   axios
     .get(teamApi + team)
     .then(response => {
       console.log(response);
+      const badgeTeam = document.querySelector('.team-logo');
+      badgeTeam.src = response.data.teams[0].strTeamBadge;
+      const teamGi = document.querySelector('#team-gi');
+      teamGi.innerHTML = response.data.teams[0].strDescriptionEN;
+      const teamSi = document.querySelector('#team-si');
+      teamSi.innerHTML = response.data.teams[0].strStadiumDescription;
+      const teamStadium = document.querySelector('.stadium');
+      teamStadium.src = response.data.teams[0].strStadiumThumb;
+      // const teamNetwork = document.querySelector('.team-social-network');
+      // aElement.href = response.data.teams[0].strWebsite;
+      // teamNetwork.appendChild(aElement.href);
     })
     .catch(error => {
       console.log(error);
